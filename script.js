78068248d3d59625bc84c3e97130a9a1779e1b19
timovibes <timothyator744@gmail.com>
2025-10-25 15:10:36 +0300

    added a comment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//displays message showing which image was clicked
 function showMessage(imageName) {
     const message = document.getElementById("message");
     message.textContent = `You clicked on the ${imageName} image!`;
